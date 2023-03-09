7878d3ec586b4c6297a4cd6b7490358b24294a03
Ivan-Oliver <ivanoliverfunes15@gmail.com>
2023-03-09 14:06:11 +0100

    feat finished base

---
+++ harrypoterweb/src/views/Welcome/index.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react'
+import {
+  ButtonBoxes,
+ 
+  WelcomeContainer,
+} from './styles'
+
+const Welcome: FC = () => {
+
+  return (
+    <WelcomeContainer>
+      <ButtonBoxes>
+      </ButtonBoxes>
+    </WelcomeContainer>
+  )
+}
+
+export default Welcome
---
+++ harrypoterweb/src/views/Welcome/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const WelcomeContainer = styled.div`
+background-color: red;
+background-repeat: no-repeat;
+background-size: cover;
+background-position: 100% 100%;
+width: 100vw;
+height:100vh;
+align-items: center;
+display: flex;
+justify-content: center;
+`
+export const ButtonBoxes = styled.div`
+    position: absolute;
+    top: 40vh;
+    flex-direction:column;
+    margin-left: 90px;
+    display: flex;
+`
+
+
+
+
