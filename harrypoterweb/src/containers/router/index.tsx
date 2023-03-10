b691f108ebb83faccaaf4362aa2ae06b490fa038
Ivan-Oliver <ivanoliverfunes15@gmail.com>
2023-03-10 17:38:26 +0100

    protected routes implemented

--- harrypoterweb/src/containers/router/index.tsx
+++ harrypoterweb/src/containers/router/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation, Navigate} from "react-router-dom";
+import { getToken } from "../../services/storage";
 import SignupForm from "../../views/Auth/Signup";
 import LoginForm from "../../views/Auth/Login";
 import Categories from '../../views/Categories';
@@ -15,9 +16,36 @@ import Profile from "../../views/Profile";
 
 
 const Router: FC = () => {
+
+    const ProtectedRoutes = ({ children }: { children: JSX.Element }) => {
+        const token = getToken();
+        const location = useLocation();
+    
+        if (!token || token === null) {
+          return <Navigate to="/" replace state={{ from: location }} />;
+        }
+        return children;
+      };
+      const HandleSession = ({ children }: { children: JSX.Element }) => {
+        const token = getToken();
+        const location = useLocation();
+    
+        if (token) {
+          if (
+            location.pathname === "/signup" ||
+            location.pathname === "/login" ||
+            location.pathname === "/"
+          ) {
+            return <Navigate to="/welcome" replace state={{ from: location }} />;
+          }
+        }
+        return children;
+    };
+
     return (
         <BrowserRouter>
         <Routes>
+            <Route path="/" element={<HandleSession><Welcome /></HandleSession>}></Route>
             <Route path='/welcome' element={<Welcome/>}/>
             <Route path='/categories' element={<Categories/>}/>
             <Route path='/signup' element={<SignupForm/>}/>
