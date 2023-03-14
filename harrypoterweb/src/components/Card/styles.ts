4a6bb2202ad01b207b6d62a2e74cf7c718f50b9a
Manuel Molina Ramirez <manuel.molina.ramirez1991@gmail.com>
2023-03-14 19:38:27 +0100

    styles: styles

--- harrypoterweb/src/components/Card/styles.ts
+++ harrypoterweb/src/components/Card/styles.ts
@@ -1,22 +1,28 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
 export const Container = styled.div`
- 
+ display: flex;
+ flex-flow: column;
+ box-shadow: 0px 0px 10px 10px black;
+ margin: 20px;
+ padding: 15px;
+ width: 200px;
 `;
-
 export const Image = styled.img`
-
+ height:150px;
+ border: solid 1px white
 `;
-
 export const DetailsButton = styled(Link)`
-
+margin-top: 10px;
+  padding: 10px;
+  color:white;
+  text-decoration: none;
+  &:hover{
+    background-color: white;
+    color: black;
+  }
 `;
-
 export const Description = styled.p`
- 
 `;
-
 export const DescriptionApi = styled.p`
- 
 `;
--- harrypoterweb/src/views/Spells/styles.ts
+++ harrypoterweb/src/views/Spells/styles.ts
@@ -1,26 +1,55 @@
 import styled from "styled-components";
 
 export const App = styled.div`
-
+  text-align: center;
+  background-color: red;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
 `;
 
 export const Container = styled.div`
-  
+  padding: 5em;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 24px;
 `;
 export const SyncButton = styled.button`
-  
+  width: 90px;
+  height: 40px;
+  border-radius: 5px;
+  margin: 10px;
+  cursor: pointer;
 `;
 export const ButtonPreview = styled.button`
- 
+  width: 90px;
+  height: 40px;
+  border-radius: 5px;
+  margin: 10px;
+  cursor: pointer;
 `;
 export const ButtonNext = styled.button`
- 
+  width: 90px;
+  height: 40px;
+  border-radius: 5px;
+  margin: 10px;
+  cursor: pointer;
 `;
 export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const DeleteButton = styled.button`
- 
+  width: 70px;
+  height: 30px;
+  border-radius: 5px;
+  margin: 10px;
+  cursor: pointer;
 `;
 
 export const ButtonBack = styled.button`
@@ -33,6 +62,7 @@ export const ButtonSearch = styled.button`
 `
 
 export const FotterPage = styled.footer`
+background-color: black;
 `
 export const ButtonRemove = styled.button`
 `
